d63498be157d2a5b0329d0eeb20eb0cbbc65c2bb
Miguel E dos Santos <miguel.santos@sieve.com.br>
2015-12-16 19:55:31 -0200

    Some stats working

--- stats.py
+++ stats.py
@@ -1,13 +1,14 @@
+from numpy.core.tests.test_numerictypes import read_values_nested
 import platform
 from multiprocessing import cpu_count
 import json
+import csv
+import copy
+from datetime import datetime
+from os import path
 
-# Linux distro
-# Kernel Version
-# CPU
-# MEMORY
-# HD
 # Network
+# HD
 
 
 def host_stats():
@@ -18,17 +19,23 @@ def host_stats():
             'cpu_count': cpu_count(),
             'model_name': cpu_model_name()
         },
-        'memory': ram_stats()
+        'memory': memory_stats(),
+        'network': network_stats()
     }
 
 
-def ram_stats():
+def memory_stats():
     _meminfo=dict()
 
     with open('/proc/meminfo') as f:
         for line in f:
-            _meminfo[line.split(':')[0]] = line.split(':')[1].strip()
-    return _meminfo
+            _meminfo[line.split(':')[0]] = line.split(':')[1].strip().split(' ')[0]
+    return {
+        'memory_total': _meminfo['MemTotal'],
+        'memory_free': _meminfo['MemFree'],
+        'swap_total': _meminfo['SwapTotal'],
+        'swap_free': _meminfo['SwapFree']
+    }
 
 
 def cpu_model_name():
@@ -40,3 +47,77 @@ def cpu_model_name():
                 if line.rstrip('\n').startswith('model name'):
                     model_name = line.rstrip('\n').split(':')[1]
                     return model_name
+
+
+def network_stats():
+
+    _tmpdict = {
+        'recieve': {
+            'bytes': 0,
+            'packets': 0,
+            'errors': 0
+        },
+        'transmit': {
+            'bytes': 0,
+            'packets': 0,
+            'errors': 0
+        }
+    }
+
+    data = {}
+
+    network_stats_file = '/tmp/network_stats'
+
+    last_stats = False
+    last_stats_time = False
+
+    if path.isfile(network_stats_file):
+        last_stats_time = datetime.fromtimestamp(path.getmtime(network_stats_file))
+
+        with open(network_stats_file, 'r') as _file:
+            last_stats = json.loads(_file.read())
+
+    with open('/proc/net/dev', 'r') as _file:
+        next(_file)
+        next(_file)
+
+        for line in _file:
+            line = list(filter(None, line.strip().split(' ')))
+
+            _interface = line[0].replace(':', '')
+
+            total_received_bytes = line[1]
+            total_transmitted_bytes = line[9]
+
+            print(total_received_bytes+':'+last_stats[_interface]['receive']['total_bytes'])
+
+            avg_received_bytes = 0
+            avg_transmitted_bytes = 0
+
+            if last_stats_time:
+                seconds = (datetime.now() - last_stats_time).total_seconds()
+                avg_received_bytes = int((int(total_received_bytes) - int(last_stats[_interface]['receive']['total_bytes'])) / seconds)
+                avg_received_bytes = int(avg_received_bytes/1024)
+
+            data[_interface] = {
+                'receive': {
+                    'total_bytes': total_received_bytes,
+                    'total_packets': line[2],
+                    'total_errors': line[3],
+                    'avg': avg_received_bytes
+                },
+                'transmit': {
+                    'total_bytes': total_transmitted_bytes,
+                    'total_packets': line[10],
+                    'total_errors': line[11],
+                    'avg': avg_transmitted_bytes
+                }
+            }
+
+    with open(network_stats_file, 'w+') as dumpfile:
+        dumpfile.write(json.dumps(data, sort_keys=True, indent=4))
+
+    return data
+
+if __name__ == "__main__":
+    print(json.dumps(host_stats(), sort_keys=True, indent=4))
