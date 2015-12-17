d825b485935068c7330afc6d2f5ebcf95cfb7e46
Miguel E dos Santos <miguel.santos@sieve.com.br>
2015-12-17 15:10:03 -0200

    Added disk stats

--- stats.py
+++ stats.py
@@ -5,7 +5,8 @@ import json
 import csv
 import copy
 from datetime import datetime
-from os import path
+from os import path, statvfs
+from math import pow
 
 # Network
 # HD
@@ -20,7 +21,8 @@ def host_stats():
             'model_name': cpu_model_name()
         },
         'memory': memory_stats(),
-        'network': network_stats()
+        'network': network_stats(),
+        'disk': disk_stats()
     }
 
 
@@ -50,32 +52,18 @@ def cpu_model_name():
 
 
 def network_stats():
-
-    _tmpdict = {
-        'recieve': {
-            'bytes': 0,
-            'packets': 0,
-            'errors': 0
-        },
-        'transmit': {
-            'bytes': 0,
-            'packets': 0,
-            'errors': 0
-        }
-    }
-
     data = {}
 
     network_stats_file = '/tmp/network_stats'
 
-    last_stats = False
-    last_stats_time = False
-
-    if path.isfile(network_stats_file):
-        last_stats_time = datetime.fromtimestamp(path.getmtime(network_stats_file))
-
-        with open(network_stats_file, 'r') as _file:
-            last_stats = json.loads(_file.read())
+    # last_stats = False
+    # last_stats_time = False
+    #
+    # if path.isfile(network_stats_file):
+    #     last_stats_time = datetime.fromtimestamp(path.getmtime(network_stats_file))
+    #
+    #     with open(network_stats_file, 'r') as _file:
+    #         last_stats = json.loads(_file.read())
 
     with open('/proc/net/dev', 'r') as _file:
         next(_file)
@@ -89,28 +77,28 @@ def network_stats():
             total_received_bytes = line[1]
             total_transmitted_bytes = line[9]
 
-            print(total_received_bytes+':'+last_stats[_interface]['receive']['total_bytes'])
+            #print(total_received_bytes+':'+last_stats[_interface]['receive']['total_bytes'])
 
-            avg_received_bytes = 0
-            avg_transmitted_bytes = 0
-
-            if last_stats_time:
-                seconds = (datetime.now() - last_stats_time).total_seconds()
-                avg_received_bytes = int((int(total_received_bytes) - int(last_stats[_interface]['receive']['total_bytes'])) / seconds)
-                avg_received_bytes = int(avg_received_bytes/1024)
+            # avg_received_bytes = 0
+            # avg_transmitted_bytes = 0
+            #
+            # if last_stats_time:
+            #     seconds = (datetime.now() - last_stats_time).total_seconds()
+            #     avg_received_bytes = int((int(total_received_bytes) - int(last_stats[_interface]['receive']['total_bytes'])) / seconds)
+            #     avg_received_bytes = int(avg_received_bytes/1024)
 
             data[_interface] = {
                 'receive': {
                     'total_bytes': total_received_bytes,
                     'total_packets': line[2],
                     'total_errors': line[3],
-                    'avg': avg_received_bytes
+                    'avg': 0
                 },
                 'transmit': {
                     'total_bytes': total_transmitted_bytes,
                     'total_packets': line[10],
                     'total_errors': line[11],
-                    'avg': avg_transmitted_bytes
+                    'avg': 0
                 }
             }
 
@@ -118,6 +106,20 @@ def network_stats():
         dumpfile.write(json.dumps(data, sort_keys=True, indent=4))
 
     return data
+
+
+def disk_stats():
+    stats = statvfs('/')
+
+    data = {
+        '/': {
+            'total': int((stats.f_bsize * stats.f_blocks) / pow(1024, 2)),
+            'free': int((stats.f_bsize * stats.f_bfree) / pow(1024, 2))
+        }
+    }
+
+    return data
+
 
 if __name__ == "__main__":
     print(json.dumps(host_stats(), sort_keys=True, indent=4))
